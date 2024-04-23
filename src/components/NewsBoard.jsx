09482e8ded621fae1774ce9a11cebddb80d51978
AmanRaj1999 <51755316+AmanRaj1999@users.noreply.github.com>
2024-04-23 11:12:00 -0700

    changes in apiKey variable

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -6,9 +6,7 @@ const NewsBoard = ({category}) => {
     const [articles,setArticles] = useState([]);
 
     useEffect(()=>{
-        // let url = ` https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
-        
-        let url = ` https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=2642277cb03c4a2480be41d08ff23b89`;
+        let url = ` https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles))
     },[category])
 
